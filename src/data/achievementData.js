29d8f61ae7af092f22116b22d8bbd9db66aa61a1
Emily Newton <47044049+emilyrn18@users.noreply.github.com>
2023-10-14 21:37:23 +0000

    Added publication data

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,30 +1,22 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'Designing for Social Interactions in a Virtual Art Gallery',
+            details : 'Authors: Polys, N., Roshan, S., Newton, E., Narula, M., & Thai. Abstract: The dawn of a new digital world has emerged with new ways to communicate and collaborate with other people across the globe. Metaverses and Mirror Worlds have broadened our perspectives on the ways we can utilize 3D virtual environments. DOI: https://doi.org/10.1145/3564533.3564562',
+            date : 'November 2022',
+            field : 'The 27th International Conference on 3D Web Technology, 1–9',
+            image : ''
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Toward a Design Theory of Game-Mediated Social Experiences - A Study of Among Us',
+            details : 'Authors: Haqq, D., Saaty, M., Rukaj, J. T., Marulkar, S., Israel, J., Newton, E., Patel, R., Tan, S., & McCrickard, D. S. Abstract: The COVID-19 pandemic has greatly affected face-to-face social interaction with and among relational partners - relatives, friends, and others. Prior to the pandemic, many people relied on face-to-face social play experiences to help them maintain relationships and satisfy relatedness needs. However, growing coronavirus-related concerns have made such activities unwelcome or inaccessible, leading many to turn to technology-mediated experiences, as a safer alternative means of supporting recreational play with non-proximal relational partners. DOI: https://doi.org/10.1145/3450337.3483469',
+            date : 'October 2021',
+            field : 'CHI PLAY \'21, 222-227',
+            image : ''
         },
-        {
-            id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-        }
 
     ]
 }
