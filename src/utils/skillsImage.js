96cd8b12a8b8b925f9cad7a174e6da04220115a3
Emily Newton <47044049+emilyrn18@users.noreply.github.com>
2023-10-14 16:32:55 +0000

    Adding skill image code

--- src/utils/skillsImage.js
+++ src/utils/skillsImage.js
@@ -73,9 +73,24 @@ import sketch from '../assets/svg/skills/sketch.svg'
 import unity from '../assets/svg/skills/unity.svg'
 import wolframalpha from '../assets/svg/skills/wolframalpha.svg'
 
-import haskell from '../assets/svg/skills/haskell.svg'
-
 import canva from '../assets/svg/skills/canva.svg'
+
+import haskell from '../assets/svg/skills/haskell.svg'
+import eclipse from '../assets/svg/skills/eclipse.svg'
+import maya from '../assets/svg/skills/maya.svg'
+import pascal from '../assets/svg/skills/pascal.svg'
+import windows from '../assets/svg/skills/windows.svg'
+import macos from '../assets/svg/skills/macos.svg'
+import ubuntu from '../assets/svg/skills/ubuntu.svg'
+import centos from '../assets/svg/skills/centos.svg'
+import debian from '../assets/svg/skills/debian.svg'
+import pios from '../assets/svg/skills/pios.svg'
+import sass from '../assets/svg/skills/sass.svg'
+import nodejs from '../assets/svg/skills/nodejs.svg'
+import express from '../assets/svg/skills/express.svg'
+import androidstudio from '../assets/svg/skills/androidstudio.svg'
+import virtualbox from '../assets/svg/skills/virtualbox.svg'
+import vmware from '../assets/svg/skills/vmware.svg'
 
 
 export const skillsImage = (skill) => {
@@ -227,10 +242,40 @@ export const skillsImage = (skill) => {
             return unity;
         case 'wolframalpha':
             return wolframalpha;
-        case 'haskell':
-            return haskell;
         case 'canva':
             return canva;
+        case 'haskell':
+            return haskell;
+        case 'eclipse':
+            return eclipse;
+        case 'maya': 
+            return maya;
+        case 'pascal': 
+            return pascal;
+        case 'windows': 
+            return windows;
+        case 'macos': 
+            return macos;
+        case 'ubuntu': 
+            return ubuntu;
+        case 'centos': 
+            return centos;
+        case 'debian': 
+            return debian;
+        case 'pios': 
+            return pios;
+        case 'sass': 
+            return sass;
+        case 'nodejs': 
+            return nodejs;
+        case 'express': 
+            return express;
+        case 'androidstudio': 
+            return androidstudio;
+        case 'virtualbox': 
+            return virtualbox;
+        case 'vmware': 
+            return vmware;
         default:
             break;
     }
