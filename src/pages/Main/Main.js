a545f81c3214a7347f99abb0dfc192f5a2c4dd2a
emilyrn18 <emilyrn18@vt.edu>
2024-01-04 16:55:50 -0500

    removed blog from main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Blog, Education, Experience, Contacts, Projects, Achievement } from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Education, Experience, Contacts, Projects, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
@@ -19,7 +19,6 @@ function Main() {
             <Experience />
             <Projects />
             <Achievement />
-            <Blog />
             <Contacts />
             <Footer />
         </div>
